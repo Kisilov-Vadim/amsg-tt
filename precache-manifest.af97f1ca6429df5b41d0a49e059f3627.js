1bdb7cbae8dd2a760042758210b53680e312f8e4
VadimK <djvadya16@gmail.com>
2019-12-10 14:26:45 +0200

    Updates

---
+++ precache-manifest.af97f1ca6429df5b41d0a49e059f3627.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa85f8f26b5b497cbf10466ee2a3a262",
+    "url": "/amsg-tt/index.html"
+  },
+  {
+    "revision": "778eececf398cc1fe239",
+    "url": "/amsg-tt/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a561c0066032f1cb4640",
+    "url": "/amsg-tt/static/js/2.b39885ff.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/amsg-tt/static/js/2.b39885ff.chunk.js.LICENSE"
+  },
+  {
+    "revision": "778eececf398cc1fe239",
+    "url": "/amsg-tt/static/js/main.6cf6d69d.chunk.js"
+  },
+  {
+    "revision": "afc5d8b7764d8dcc0309",
+    "url": "/amsg-tt/static/js/runtime-main.3d6fc401.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/amsg-tt/static/media/logo.5d5d9eef.svg"
+  }
+]);
