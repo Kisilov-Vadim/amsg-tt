3bcfd442d8b0680da21bb2edfad61e7de7767163
VadimK <djvadya16@gmail.com>
2019-12-11 14:36:00 +0200

    done

--- src/Body/Projects/ProjectCard/ProjectCard.js
+++ src/Body/Projects/ProjectCard/ProjectCard.js
@@ -4,7 +4,7 @@ import './ProjectCard.scss';
 const ProjectCard = ({img, id}) => (
   <div className="projectCard">
     <img src={img} alt="project" className={`projectCard ${id > 6 ? 'delete' : null}`} />
-    <div className={`projectCard ${id > 6 ? 'delete' : null}`}>
+    <div className={`projectCard__info ${id > 6 ? 'delete' : null}`}>
       <span className='projectCard__info-title'>Project Name</span>
       <p className='projectCard__info-body'>User Interface Design</p>
     </div>
